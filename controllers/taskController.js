1ffa33db7c80b5dd5e4fdbbd7e1e21ea96ecf2b1
codephillip <codephillip@gmail.com>
2020-07-08 12:10:20 +0300

    added create task, added pug lib

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -1,5 +1,6 @@
 var Task = require('../models/task')
 
+
 exports.get_all_tasks = (req, res) => {
     Task.find()
         .exec((err, tasks) => {
@@ -8,4 +9,18 @@ exports.get_all_tasks = (req, res) => {
             }
             res.json({'tasks': tasks});
         });
+}
+
+exports.create_task = (req, res, next) => {
+    var task = new Task({
+        'title': req.body.title,
+        'message': req.body.message,
+        'due_date': req.body.due_date
+    });
+
+    task.save((err) => {
+        if (err)
+            next(err);
+        res.json({'task:': task});
+    });
 }
--- routes/tasks.js
+++ routes/tasks.js
@@ -3,5 +3,6 @@ var router = express.Router();
 var taskController = require('../controllers/taskController')
 
 router.get('/tasks', taskController.get_all_tasks);
+router.post('/tasks', taskController.create_task);
 
 module.exports = router;
